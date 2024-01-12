0ad88d438bf5913fe64fe1844d3c65ae5f0bd4c9
daslu <daniel.slutsky@gmail.com>
2024-01-12 20:14:25 +0200

    API changes: dashes instread of slashes

--- README.md
+++ README.md
@@ -33,6 +33,6 @@ Using [use-package](https://github.com/jwiegley/use-package) and [vc-use-package
 The package offers the following functions, that you may wish to create keybindings for:
 |name|function|
 |--|--|
-|`kind-portal/open-if-needed`|Open the Portal view if you haven't yet.|
-|`kind-portal/send-last-sexp`|Submit the Clojure form in last s-expression to Portal, adapted for Kindly|
-|`kind-portal/send-defun-at-point`|Submit the Clojure form at the [defun-at-point](https://www.emacswiki.org/emacs/ThingAtPoint) to Portal, adapted for Kindly|
+|`kind-portal-open-if-needed`|Open the Portal view if you haven't yet.|
+|`kind-portal-send-last-sexp`|Submit the Clojure form in last s-expression to Portal, adapted for Kindly|
+|`kind-portal-send-defun-at-point`|Submit the Clojure form at the [defun-at-point](https://www.emacswiki.org/emacs/ThingAtPoint) to Portal, adapted for Kindly|
--- kind-portal.el
+++ kind-portal.el
@@ -24,14 +24,14 @@
 
 ;;; Code:
 
-(defun kind-portal/open-if-needed ()
+(defun kind-portal-open-if-needed ()
   (interactive)
   (cider-interactive-eval "
     (require '[scicloj.kind-portal.v1.api])
     (scicloj.kind-portal.v1.api/open-if-needed)")
   t)
 
-(defun kind-portal/send (code)
+(defun kind-portal-send (code)
   (cider-interactive-eval
    (concat "
      (require '[portal.api]
@@ -39,13 +39,13 @@
      (portal.api/clear)
      (scicloj.kind-portal.v1.api/kindly-submit-context {:form (quote " code ")})")))
 
-(defun kind-portal/send-last-sexp ()
+(defun kind-portal-send-last-sexp ()
   (interactive)
-  (kind-portal/send (cider-last-sexp)))
+  (kind-portal-send (cider-last-sexp)))
 
-(defun kind-portal/send-defun-at-point ()
+(defun kind-portal-send-defun-at-point ()
   (interactive)
-  (kind-portal/send (thing-at-point 'defun)))
+  (kind-portal-send (thing-at-point 'defun)))
 
 (provide 'kind-portal)
 ;;; kind-portal.el ends here
